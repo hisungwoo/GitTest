687f99d8520430b0e8dda7c1f3478023d0912561
sungwoo <hisungwoo007@naver.com>
2020-02-28 12:55:31 +0900

    테스트1

--- app/src/main/java/com/example/woo/gittest/MainActivity.kt
+++ app/src/main/java/com/example/woo/gittest/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.woo.gittest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Log.d("TAG", "테스트")
     }
 }
